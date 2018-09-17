14b0cf58b01dbf5b2fa70e371a98ca78ddf0578d
ijklr <cheng754@gmail.com>
2018-09-17 02:11:14 -0400

    Asyc

---
+++ src/main/java/com/mycompany/app/AsyncTimeout.java
@@ -0,0 +1,39 @@
+package com.mycompany.app;
+
+import java.util.concurrent.CompletableFuture;
+
+public class AsyncTimeout {
+
+    String func1() {
+        CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(2000);
+                System.out.println("after sleeping, func1 is done");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.out.println("sleep messed up in func1");
+            }
+            return "from async in func1";
+        });
+
+
+        return "I am func1";
+    }
+
+    String func2() {
+        try {
+            Thread.sleep(2005);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.out.println("sleep messed up in func2");
+        }
+        return "I am func2";
+    }
+
+    public static void main(String args[]) {
+        System.out.println("hello");
+        AsyncTimeout asyncTimeout = new AsyncTimeout();
+        System.out.println(asyncTimeout.func1());
+        System.out.println(asyncTimeout.func2());
+    }
+}
